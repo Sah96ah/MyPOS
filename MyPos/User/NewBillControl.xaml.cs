2ee918347add0a3caca82331c3b545bb90326519
Saheen Ahamed <sah.96ah@gmail.com>
2020-01-15 18:54:51 +0530

    add required validators to newbill controller

--- MyPos/User/NewBillControl.xaml.cs
+++ MyPos/User/NewBillControl.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Text.RegularExpressions;
+using System.Data;
+using System.Collections;
+using System.Windows.Controls.Primitives;
 
 namespace MyPos
 {
@@ -25,12 +28,13 @@ namespace MyPos
         {
             InitializeComponent();
             fillCombo1();
-            txtCurrentDate.Text = DateTime.Now.ToShortDateString();
+            txtCurrentDate.Text = DateTime.Now.ToString("MM/dd/yyyy");
             clearAll();
+            //filldataTable();
         }
 
-        //globals
-        string unitPrice;
+        int rowindex;//for deleting datagrid row
+
         string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 
         void fillCombo1()
@@ -38,9 +42,9 @@ namespace MyPos
             using (SqlConnection con = new SqlConnection(cs))
             {
                 string query = "SELECT DISTINCT catogory FROM Stock";
-                SqlCommand cmd = new SqlCommand(query,con);
+                SqlCommand cmd = new SqlCommand(query, con);
                 con.Open();
-                SqlDataReader rd =  cmd.ExecuteReader();
+                SqlDataReader rd = cmd.ExecuteReader();
                 while (rd.Read())
                 {
                     cmbProduct.Items.Add(rd["catogory"].ToString());
@@ -56,12 +60,15 @@ namespace MyPos
             txtSubTotal.Text = "";
             txtUnitPrice.Text = "";
             cmbProductID.SelectedIndex = -1;
+            txtTotal.Text = "";
+            txtDiscount.Text = "";
         }
 
         private void cmbProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             clearAll();
 
+            //databind to cmbProductID
             if (cmbProduct.SelectedItem.ToString() == "")
             {
                 productValidator.Visibility = Visibility.Visible;
@@ -82,6 +89,8 @@ namespace MyPos
 
         private void cmbProductID_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //get description according to product id 
+            //get unit price accoding to product id
             if (cmbProductID.SelectedIndex != -1)
             {
                 int pId = int.Parse(cmbProductID.SelectedItem.ToString());
@@ -94,10 +103,9 @@ namespace MyPos
                     con.Open();
                     txtProductDes.Text = (cmd.ExecuteScalar()).ToString();
                     txtUnitPrice.Text = (cmd2.ExecuteScalar()).ToString();
-                    //because this is readonly
-                    unitPrice = (cmd2.ExecuteScalar()).ToString();
+
                 }
-            
+
             }
         }
 
@@ -123,10 +131,10 @@ namespace MyPos
             if (txtDiscount.Text != "")
             {
                 decimal dicper = decimal.Parse(txtDiscount.Text);
-                decimal total = decimal.Parse(txtSubTotal.Text)-(dicper * decimal.Parse(txtSubTotal.Text)) / 100;
-                txtTotal.Text = total.ToString(); 
+                decimal total = decimal.Parse(txtSubTotal.Text) - (dicper * decimal.Parse(txtSubTotal.Text)) / 100;
+                txtTotal.Text = total.ToString();
             }
-            
+
         }
 
         //not allow other than numeric value in text box
@@ -134,7 +142,6 @@ namespace MyPos
         {
             e.Handled = !IsTextAllowed(e.Text);
         }
-        
         private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
         private static bool IsTextAllowed(string text)
         {
@@ -143,25 +150,99 @@ namespace MyPos
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            string s1 = cmbProductID.SelectedItem.ToString();
-            string s2 = cmbProduct.SelectedItem.ToString();
-            string s3 = txtProductDes.Text;
-            decimal d1 = decimal.Parse(txtQuality.Text);
-            decimal d2 = decimal.Parse(unitPrice);
-            decimal d3 = decimal.Parse(txtDiscount.Text);
-            decimal d4 = decimal.Parse(txtSubTotal.Text);
-            decimal d5 = decimal.Parse(txtTotal.Text);
-
-            int billno = 100;
-            using (SqlConnection con = new SqlConnection(cs))
+            //to check if any empty fields available
+            if (txtDiscount.Text != "" || txtProductDes.Text != "" || txtQuality.Text != "" || txtTotal.Text != "" || txtUnitPrice.Text != ""
+            || txtSubTotal.Text != "" || cmbProduct.SelectedIndex != -1 || cmbProductID.SelectedIndex != -1)
             {
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = con;
-                cmd.CommandText = "INSERT INTO Sales (productID,catogory,quantity,unitprice,discount,subtotal,total,billno,description,billdate) VALUES ('" + s1+"','"+s2+"',"+d1+","+d2+","+d3+","+d4+","+d5+","+billno+",'"+s3+"','"+txtCurrentDate.Text+"')";
-                con.Open();
-                cmd.ExecuteNonQuery();
+                //set attribute for data grid class
+                var data = new Grid { p1 = txtProductDes.Text, p2 = txtUnitPrice.Text, p3 = txtQuality.Text, p4 = txtSubTotal.Text, p5 = txtDiscount.Text, p6 = txtTotal.Text, p7 = int.Parse(cmbProductID.SelectedItem.ToString())};
 
+                grdBillDetails.Items.Add(data);
+                clearAll();
             }
         }
+        
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            string dt = DateTime.Now.ToString();
+            string date = Regex.Replace(dt, @"[^0-9]", "");
+            long bn = long.Parse(date);
+
+            //get each cell values in datagrid
+            for (int i = 0; i < grdBillDetails.Items.Count; i++)
+            {
+                //get each colomn value 
+                TextBlock description = grdBillDetails.Columns[1].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock unitPrice = grdBillDetails.Columns[2].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock Quantity = grdBillDetails.Columns[3].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock Subtotal = grdBillDetails.Columns[4].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock Discount = grdBillDetails.Columns[5].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock Total = grdBillDetails.Columns[6].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+                TextBlock productId = grdBillDetails.Columns[0].GetCellContent(grdBillDetails.Items[i]) as TextBlock;
+
+               
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "INSERT INTO Sales (billno,productID,description,unitprice,quantity,subtotal,discount,total,date) VALUES (" + bn + "," + int.Parse(productId.Text) + ",'" + description.Text + "'," + decimal.Parse(unitPrice.Text) + "," + int.Parse(Quantity.Text) + "," + decimal.Parse(Subtotal.Text) + "," + decimal.Parse(Discount.Text) + "," + decimal.Parse(Total.Text) + ",'"+txtCurrentDate.Text+ "')";
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        
+        private int BillNumberGenerator(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        //delete row from grid 
+        private void grdBillDetails_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            if (e.AddedItems != null && e.AddedItems.Count > 0)
+            {
+                DataGridRow row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromItem(e.AddedItems[0]);
+                if (row != null)
+                {
+                    btnAdd.IsEnabled = false;
+                    rowindex = row.GetIndex();
+                }
+            }
+        }
+        static T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null) child = GetVisualChild<T>(v);
+                if (child != null) break;
+            }
+            return child;
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            grdBillDetails.Items.RemoveAt(rowindex);
+            btnAdd.IsEnabled = true;
+        }
+    }
+
+    //class which add items to database
+    public class Grid
+    {
+        public string p1 { get; set; }
+        public string p2 { get; set; }
+        public string p3 { get; set; }
+        public string p4 { get; set; }
+        public string p5 { get; set; }
+        public string p6 { get; set; }
+        public int p7 { get; set; }
+
     }
 }
--- MyPos/User/SalesReturnControl.xaml.cs
+++ MyPos/User/SalesReturnControl.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace MyPos
 {
@@ -21,9 +24,42 @@ namespace MyPos
         public SalesReturnControl()
         {
             InitializeComponent();
-       
         }
-        
+
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            long bno = long.Parse(txtbillNo.Text);
+            int pID = int.Parse(txtProductID.Text);
+
+            using (SqlConnection con = new SqlConnection(cs)) 
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT date,quantity,unitprice,total FROM Sales WHERE billno=" + bno + " AND productID="+pID;
+                cmd.Connection = con;
+                con.Open();
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    txtDate.Text = rd.GetValue(0).ToString();
+                    txtQuantity.Text = rd.GetValue(1).ToString();
+                    txtUnitPrice.Text = rd.GetValue(2).ToString();
+                    txtTotal.Text = rd.GetValue(3).ToString();
+                }
+            }
+
+        }
+        //not allow other than numeric value in text box
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
     }
 }
 
